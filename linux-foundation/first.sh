a0881e3f34698e31e2ea33a11591777d1f6baf6f
mohi2006august <nizamuddinnumaan822@gmail.com>
2026-07-31 23:25:01 +0530

    Permission and ownership(Day-5)

---
+++ linux-foundation/first.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "===== Linux Basic Script ====="
+
+name="Mohiuddin"
+
+echo "Hello, $name!"
+echo "Current user: $USER"
+echo "Current directory:"
+pwd
+
+echo "Files in current directory:"
+ls
+
+echo "===== Script Finished ====="
